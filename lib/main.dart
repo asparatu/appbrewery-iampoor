b949bbdcc6aa10325341f37a56f5fb6c5e7c3295
santachph <asparatu@hotmail.com>
2020-05-15 16:53:19 +0800

    Updated Project UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white70,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I am Poor'),
+          ),
+          backgroundColor: Colors.blue[600],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/coal.png'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
